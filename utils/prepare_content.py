55721970596b614727415debe6b961bbde708b4c
pashtetx <vadimplej43@gmail.com>
2024-01-01 01:53:11 +0200

    user doesn't exits error exception 2

--- utils/prepare_content.py
+++ utils/prepare_content.py
@@ -2,13 +2,13 @@ from db.models import User
 from datetime import datetime
 
 
-def prepare_response_text(text: str, sender: User, is_subscribe: bool = False) -> str:
+def prepare_response_text(text: str, sender: User, is_visible: bool = False) -> str:
     
     username = "@" + sender.username if sender.username \
         else f"{sender.first_name if sender.first_name else ''} {sender.last_name if sender.last_name else ''}"
     
     response = (
-        f"❤ Вам пришло сообщение от {f'{username}' if is_subscribe else 'Анонимного пользователя'}:\n\n",
+        f"❤ Вам пришло сообщение от {f'{username}' if is_visible else 'Анонимного пользователя'}:\n\n",
         f"<b>{text}</b>"
     )
 
@@ -19,10 +19,7 @@ def prepare_start_text(current_user: User):
     response = (
         f"Привет, это бот для анонимных валентинок, чтобы отправлять сообщения анонимно. 💌\n\n",
         f"- ❓ Чтобы получать анонимные сообщения, нужно сгенерировать ссылку /link и с помощью этой ссылки люди будут писать вам анонимные сообщения.\n",
-        f"- 🔔 В боте есть возможность подписки с помощью подписки можно смотреть кто отправил сообщение.\n\n",
-        f"✨ Подписка: {'Есть' if current_user.subscribed else 'Нету'}\n",
-        f"{'Подписка истёчет в: {expire}'.format(expire=current_user.expire_in.strftime(format='%d.%m, %Y')) if current_user.subscribed else ''}\n"
-        f"📦 <i>Это бета версия бота! Некоторые функции не работают!</i>"   
+        f"📦 <i>Это бета версия бота! Некоторые функции не работают!</i>",
     )
 
     return "".join(response)
